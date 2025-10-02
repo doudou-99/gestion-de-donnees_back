113cc4a3183914a60323865f4e8808c24e245932
Nassim <latrach.nassim@gmail.com>
2025-10-02 11:06:06 +0200

    ass de package

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,40 +1,42 @@
 import { PrismaClient } from '@prisma/client';
-import * as argon2 from "argon2";
+import * as argon2 from 'argon2';
 import { fakerFR as faker } from '@faker-js/faker';
 
 const prisma = new PrismaClient();
 
 const upsertUser = async () => {
-    const pass = faker.internet.password({pattern: /^[A-Za-z0-9*.!@#$%^&(){}[]:;<>,.?\/~_+-=|\]*$/});
-    const user = await prisma.user.upsert({
-        where: {email: faker.internet.email()},
-        update: {},
-        create: {
-            email: faker.internet.email({allowSpecialCharacters: true}),
-            password: await argon2.hash(pass),
-            username: faker.internet.username(),
-            address: faker.location.streetAddress(),
-            status: "CONFIRMED",
-            additionalAddress: faker.location.secondaryAddress(),
-            zipCode: faker.location.zipCode(),
-            extraEmail: faker.internet.email({allowSpecialCharacters: true})
-        }
-    })
-    console.log(user, pass);
-}
+  const pass = faker.internet.password({
+    pattern: /^[A-Za-z0-9*.!@#$%^&(){}[]:;<>,.?\/~_+-=|\]*$/,
+  });
+  const user = await prisma.user.upsert({
+    where: { email: faker.internet.email() },
+    update: {},
+    create: {
+      email: faker.internet.email({ allowSpecialCharacters: true }),
+      password: await argon2.hash(pass),
+      username: faker.internet.username(),
+      address: faker.location.streetAddress(),
+      status: 'CONFIRMED',
+      additionalAddress: faker.location.secondaryAddress(),
+      zipCode: faker.location.zipCode(),
+      extraEmail: faker.internet.email({ allowSpecialCharacters: true }),
+    },
+  });
+  console.log(user, pass);
+};
 
 const main = async (nbUser: number) => {
-    for (let i = 0; i < nbUser; i++) {
-        upsertUser();
-    }
-}
+  for (let i = 0; i < nbUser; i++) {
+    upsertUser();
+  }
+};
 
 main(2)
-.then(async () => {
+  .then(async () => {
     await prisma.$disconnect();
-})
-.catch(async (err) => {
-    console.error(err)
-    await prisma.$disconnect()
-    process.exit(1)
-});
+  })
+  .catch(async (err) => {
+    console.error(err);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
