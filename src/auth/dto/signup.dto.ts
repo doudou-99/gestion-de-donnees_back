0c9f27d35ca3a0c0f85884917ff239826854fd24
doudou-99 <momarndoye1999@gmail.com>
2025-11-08 17:01:08 +0100

    Add getOwnerFile and Swagger in the API

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString, IsNotEmpty, Length, IsStrongPassword, IsOptional, IsPostalCode } from "class-validator";
 
 export class SignupDto {
+    @ApiProperty({required: true, uniqueItems: true})
     @IsEmail()
     email: string;
     
+    @ApiProperty({required: true, minLength: 5})
     @IsString()     
     @IsNotEmpty()    
     @Length(5)     
     username: string;     
       
+    @ApiProperty({required: true, minLength: 8})
     @IsString()     
     @IsNotEmpty()     
     @IsStrongPassword({        
@@ -18,22 +22,26 @@ export class SignupDto {
       minUppercase: 1    
     })    
     password: string;    
-      
+    
+    @ApiProperty({required: true, minLength:5})
     @IsString()     
     @IsNotEmpty()     
     @Length(5)     
     address: string;      
     
+    @ApiProperty({required: false, minLength: 5})
     @IsString()     
     @IsNotEmpty()     
     @Length(5)     
     @IsOptional()     
     additionalAddress?: string;       
     
+    @ApiProperty({required: true})
     @IsNotEmpty()     
     @IsString()    
     zipCode: string;       
-    
+  
+    @ApiProperty({required: false})
     @IsEmail()     
     @IsOptional()     
     extraEmail?: string; 
