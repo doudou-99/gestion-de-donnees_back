bff0e296825cc55c72d1156e14f2b89e1d65d2d8
Momar NDOYE <momarndoye1999@gmail.com>
2026-04-22 10:32:31 +0200

    Edit init-rds

--- gateway/init-rds.sh
+++ gateway/init-rds.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mysql -h database-1.c3agsaymkxi0.eu-west-3.rds.amazonaws.com -P 3306 -u admin -p S6tDBi6vv6SW58P3FL23 --ssl-mode=VERIFY_IDENTITY --ssl-ca=./global-bundle.pem < ./init-user.sql
+mysql -h database-1.c3agsaymkxi0.eu-west-3.rds.amazonaws.com -P 3306 -u admin -p S6tDBi6vv6SW58P3FL23 gestion_file --ssl-mode=VERIFY_IDENTITY --ssl-ca=./global-bundle.pem < ./init-user.sql
