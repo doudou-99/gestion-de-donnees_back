88e70c6b5dadf49827485a78a1c1e0c5e7b3e3a6
Momar NDOYE <momarndoye1999@gmail.com>
2026-04-22 11:57:16 +0200

    Edit init-rds

--- gateway/init-rds.sh
+++ gateway/init-rds.sh
@@ -6,4 +6,6 @@ RDS_USER=admin
 RDS_PASSWORD=S6tDBi6vv6SW58P3FL23
 RDS_DATABASE=gestion_file
 
+apk add mariadb mariadb-client
+
 mysql -h $RDS_HOST -P $RDS_PORT -u $RDS_USER -p $RDS_PASSWORD $RDS_DATABASE --ssl-mode=VERIFY_IDENTITY --ssl-ca=./global-bundle.pem < ./init-user.sql
