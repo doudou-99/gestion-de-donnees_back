bc743aa5506165cb1a5fbf50597bcf0b7931dd81
Momar NDOYE <momarndoye1999@gmail.com>
2026-04-22 11:16:23 +0200

    Edit init-rds

--- gateway/init-rds.sh
+++ gateway/init-rds.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
+set -euo pipefail
 RDS_HOST=database-1.c3agsaymkxi0.eu-west-3.rds.amazonaws.com
 RDS_PORT=3306
 RDS_USER=admin
 RDS_PASSWORD=S6tDBi6vv6SW58P3FL23
 RDS_DATABASE=gestion_file
-
-sudo apt-get install -y mysql-client
 
 mysql -h $RDS_HOST -P $RDS_PORT -u $RDS_USER -p $RDS_PASSWORD $RDS_DATABASE --ssl-mode=VERIFY_IDENTITY --ssl-ca=./global-bundle.pem < ./init-user.sql
