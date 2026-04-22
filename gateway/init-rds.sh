1a6296a65f72ba65e4ca9febb9b5c84fa9f1ec04
Momar NDOYE <momarndoye1999@gmail.com>
2026-04-22 10:47:36 +0200

    Edit init-rds

--- gateway/init-rds.sh
+++ gateway/init-rds.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+RDS_HOST=database-1.c3agsaymkxi0.eu-west-3.rds.amazonaws.com
+RDS_USER=admin
+RDS_PASSWORD=S6tDBi6vv6SW58P3FL23
+RDS_DATABASE=gestion_file
 
-mysql -h database-1.c3agsaymkxi0.eu-west-3.rds.amazonaws.com -P 3306 -u admin -p S6tDBi6vv6SW58P3FL23 gestion_file --ssl-mode=VERIFY_IDENTITY --ssl-ca=./global-bundle.pem < ./init-user.sql
+aws ssm send-command \
+  --instance-ids "database-1" \
+  --document-name "AWS-RunShellScript" \
+  --parameters commands=["mysql -h $RDS_HOST -u $RDS_USER -p$RDS_PASSWORD $RDS_DATABASE --ssl-mode=VERIFY_IDENTITY --ssl-ca=./global-bundle.pem < ./init-user.sql"]
