30fd3b4cf780db08cc7c6791912c4aa0dbb1d952
Momar NDOYE <momarndoye1999@gmail.com>
2026-04-22 13:48:36 +0200

    Edit init-rds

--- gateway/init-rds.sh
+++ gateway/init-rds.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-
 RDS_HOST=database-1.c3agsaymkxi0.eu-west-3.rds.amazonaws.com
 RDS_PORT=3306
 RDS_USER=admin
 RDS_PASSWORD=S6tDBi6vv6SW58P3FL23
 RDS_DATABASE=gestion_file
-MYSQL_PWD="$RDS_PASSWORD" 
+
+export MYSQL_PWD="$RDS_PASSWORD"
+
 mysql \
   -h "$RDS_HOST" \
   -P "$RDS_PORT" \
