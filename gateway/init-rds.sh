f9c84bc7d37491f2ecd32157dc497e12ba25729b
Momar NDOYE <momarndoye1999@gmail.com>
2026-04-22 11:04:44 +0200

    Edit init-rds

--- gateway/init-rds.sh
+++ gateway/init-rds.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 RDS_HOST=database-1.c3agsaymkxi0.eu-west-3.rds.amazonaws.com
+RDS_PORT=3306
 RDS_USER=admin
 RDS_PASSWORD=S6tDBi6vv6SW58P3FL23
 RDS_DATABASE=gestion_file
 
-aws ssm send-command \
-  --instance-ids "database-1" \
-  --document-name "AWS-RunShellScript" \
-  --parameters commands=["mysql -h $RDS_HOST -u $RDS_USER -p$RDS_PASSWORD $RDS_DATABASE --ssl-mode=VERIFY_IDENTITY --ssl-ca=./global-bundle.pem < ./init-user.sql"]
+sudo apt-get install -y mysql-client
+
+mysql -h $RDS_HOST -P $RDS_PORT -u $RDS_USER -p $RDS_PASSWORD $RDS_DATABASE --ssl-mode=VERIFY_IDENTITY --ssl-ca=./global-bundle.pem < ./init-user.sql
